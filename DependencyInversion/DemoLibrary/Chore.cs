cfa1d8df464b5f7edf02384b6f201de2ef82e7d7
srkshivaraman <57571341+srkshivaraman@users.noreply.github.com>
2024-08-23 00:15:47 +0530

    Base code with out dependecy injection

---
+++ DependencyInversion/DemoLibrary/Chore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Chore
+    {
+        public string ChoreName { get; set; }
+        public Person Owner { get; set; }
+
+        public double HoursWorked { get; private set; }
+
+        public bool IsComplete { get; private set; }
+
+        public void CompleteChore()
+        {
+            IsComplete = true;
+            Logger logger = new Logger();
+            logger.Log($"Completed {ChoreName}");
+
+            Emailer emailer = new Emailer();
+            emailer.SendEmail(Owner, $"The chore {ChoreName} is complete.");
+        }
+
+        public void PerformedWork(double hours)
+        {
+            HoursWorked += hours;
+            Logger logger = new Logger();
+            logger.Log($"Performed work on {ChoreName}");
+        }
+
+    
+    }
+}
---
+++ DependencyInversion/DependencyInversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using DemoLibrary;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Person owner = new Person();
+        owner.FirstName = "Peter";
+        owner.LastName = "Parker";
+        owner.EmailAddress = "ceo@parkerindustries.com";
+        owner.PhoneNumber = "9876543210";
+
+        Chore chore = new Chore();
+        chore.ChoreName = "Clean my room";
+        chore.Owner= owner;
+
+        chore.PerformedWork(3);
+        chore.PerformedWork(1.5);
+        chore.CompleteChore();
+
+        Console.ReadLine();
+    }
+}
